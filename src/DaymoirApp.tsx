c07a899051cfcfa273ea957169dde07d5bc5a740
Yoona Oh <j.oh@rice.edu>
2022-12-21 15:16:20 -0800

    WIP: Add changes to landing page and add resource strings

--- src/DaymoirApp.tsx
+++ src/DaymoirApp.tsx
@@ -1,24 +1,40 @@
 import * as React from 'react';
 import logo from './logo.svg';
 import './DaymoirApp.css';
+import { strings } from './res/strings';
+import AppBar from '@mui/material/AppBar'; // minimizing bundle size by importing just what we need
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const resource = strings.landing;
 
 const DaymoirApp = () => {
   return (
     <div className="DaymoirApp">
       <header className="DaymoirApp-header">
-        <img src={logo} className="DaymoirApp-logo" alt="logo" />
-        <p>
-          Edit <code>src/DaymoirApp.tsx</code> and save to reload.
-        </p>
-        <a
-          className="DaymoirApp-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AppBar color="transparent">
+          <div className="DaymoirApp-appBar">
+            <span>
+              <Typography>{resource.title}</Typography>
+            </span>
+            <span className="DaymoirApp-link">
+              <Button variant="text">{resource.about}</Button>
+              <Button variant="text">{resource.login}</Button>
+              <Button variant="text">{resource.signUp}</Button>
+            </span>
+          </div>
+        </AppBar>
       </header>
+      <div className="DaymoirApp-content">
+        <div>
+          <Typography variant="h1">{resource.heading}</Typography>
+          <Typography paragraph={true}>{resource.description}</Typography>
+          <Button variant="contained">{resource.startWriting}</Button>
+        </div>
+        <div>
+          <img src={logo} className="DaymoirApp-logo" alt="logo" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/res/strings.js
@@ -0,0 +1,92 @@
+export const strings = {
+    landing: {
+        title: 'Daymoir',
+        heading: 'A low-effort journal, scrapbook, and time capsule',
+        description: 'Making memories isn\'t hard and neither should keeping them. Record everyday and once-in-a-lifetime events in your digital daily memoir.',
+        about: 'About',
+        login: 'Login',
+        signUp: 'Sign up',
+        startWriting: 'Start writing'
+    },
+    account: {
+        createAccount: 'Create account',
+        name: 'Name',
+        email: 'Email',
+        password: 'Password',
+        confirmPassword: 'Confirm password',
+        rememberMe: 'Remember me',
+        signUp: 'Sign up',
+        existingLoginPrompt: 'Already have an account?',
+        login: 'Login',
+        forgotPassword: 'Forgot password',
+        newUserPrompt: 'New to Daymoir? Sign up here',
+    },
+    sideMenu: {
+        streak: '{0} day streak',
+        numEntries: '{0} entries',
+        recent: 'Recent',
+        collections: 'Collections',
+        timeCapsule: 'Time capsule',
+        settings: 'Settings',
+        signOut: 'Sign out'
+    },
+    recent: {
+        title: 'Recent',
+        addNewEntry: 'Add new entry',
+        search: 'Search'
+    },
+    entry: {
+        newEntryTitle: 'New entry',
+        date: 'Date',
+        mood: 'Mood',
+        collection: 'Collection',
+        attach: 'Attachments',
+        add: 'Add',
+        cancel: 'Cancel',
+        editEntryTitle: 'Edit entry',
+        edit: 'Edit',
+        save: 'Save',
+        delete: 'Delete'
+    },
+    collection: {
+        title: 'Collections',
+        favorites: 'Favorites',
+        addNewCollection: 'Add new collection',
+        rename: 'Rename',
+        delete: 'Delete'
+    },
+    timeCapsule: {
+        title: 'Time capsule',
+        countdownDescription: 'until the next time capsule opening',
+        open: 'Open',
+        readyToBeOpenedDescription: 'Your time capsule is ready to be opened!',
+        startNewTimeCapsule: 'Start a new one',
+        notEnabledHeading: 'Time capsule is currently OFF.',
+        notEnabledDescription: 'If you would like to start one, go to ',
+        settings: 'Settings'
+    },
+    settings: {
+        title: 'Settings',
+        accountHeading: 'Account',
+        email: 'Email',
+        password: 'Password',
+        editAvatar: 'Edit avatar',
+        editName: 'Edit name',
+        notificationsHeading: 'Notifications',
+        browser: 'Browser',
+        desktop: 'Desktop',
+        email: 'Email',
+        time: 'Time to send daily notification',
+        timeCapsuleHeading: 'Time capsule',
+        timeCapsuleDescription: 'Hides entries until a certain date',
+        on: 'On',
+        off: 'Off',
+        nextOpenDate: 'Next opening date'
+    },
+    signOut: {
+        inProgress: 'Signing out',
+        completeHeading: 'Thank you',
+        completeDescription: 'You have successfully signed out. See you tomorrow!',
+        home: 'Home'
+    }
+};
